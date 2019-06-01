027cb76921bdca9cef68405bdb099cdf9d42decd
Ivan <kasiama@yandex.ru>
2019-06-01 10:54:55 +0300

    Initial Commit

---
+++ images/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  images
+//
+//  Created by Иван on 6/1/19.
+//  Copyright © 2019 Иван. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ images/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  images
+//
+//  Created by Иван on 6/1/19.
+//  Copyright © 2019 Иван. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
